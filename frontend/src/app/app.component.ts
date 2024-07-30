60f80c937c1fa36d71da65b616d2a7295816cc6f
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2024-07-30 19:35:33 +0300

    add header + first section of home

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AboutComponent} from "../main-components/about/about.component";
+import {ContactComponent} from "../main-components/contact/contact.component";
+import {HeaderComponent} from "../main-components/header/header.component";
+import {HomeComponent} from "../main-components/home/home.component";
+import {SkillsComponent} from "../main-components/skills/skills.component";
+import {WorkComponent} from "../main-components/work/work.component";
+import {FooterComponent} from "../main-components/footer/footer.component";
+
+const COMPONENTS = [AboutComponent , ContactComponent , HeaderComponent , HomeComponent , SkillsComponent , WorkComponent , FooterComponent]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , COMPONENTS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/main-components/header/header.component.ts
+++ frontend/src/main-components/header/header.component.ts
@@ -1,13 +1,25 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {Router, RouterModule} from "@angular/router";
 
 @Component({
   selector:'app-header',
   templateUrl:'header.component.html',
   styleUrls:['header.component.scss'],
   standalone:true,
-  imports:[CommonModule]
+  imports:[CommonModule , RouterModule]
 })
 export class HeaderComponent{
+  public logoSign = "</>"
 
+  constructor(private _router:Router) {
+  }
+
+  public goToLinkedin(): void {
+    window.open("https://www.linkedin.com/in/petrut-poenaru-dev", "_blank");
+  }
+
+  public goToGitHub(): void {
+    window.open("https://github.com/petrut-poenaru-dev", "_blank");
+  }
 }
