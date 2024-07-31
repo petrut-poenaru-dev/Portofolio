257295f2ac141ef304ecfb459c3764129bbc1abf
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2024-07-31 18:38:10 +0300

    finish works section

--- frontend/src/main-components/work/work.component.ts
+++ frontend/src/main-components/work/work.component.ts
@@ -1,13 +1,29 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {SlickCarouselModule} from "ngx-slick-carousel";
 
 @Component({
   selector:'app-work',
   templateUrl:'work.component.html',
   styleUrls:['work.component.scss'],
   standalone:true,
-  imports:[CommonModule]
+  imports:[CommonModule , SlickCarouselModule]
 })
 export class WorkComponent{
+  slideConfig = {
+    "slidesToShow": 1,
+    "slidesToScroll": 1,
+    "autoplay": false,
+    "dots":true,
+    "autoplaySpeed": 3000,
+    "arrows": true,
+  };
 
+  public goToJurist(){
+    window.open("https://juristhub.ro/", "_blank")
+  }
+
+  public goToFloAuto(){
+    window.open("  https://floautolease.ro/acasa", "_blank")
+  }
 }
